e324fc99f59e126efd771506db766cf70214f096
Vincent Le <vincentle.fh@gmail.com>
2022-03-07 11:07:00 +0200

    :link: Add dev script, initialize db connection

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import express from 'express';
+import db from './db/index.js';
 
 const app = express();
 
 //Test localhost:8787 from browser
 app.get('/', (_request, response) => {
   response.send('<h1>Hello World!</h1>')
+})
+
+// Test route for getting signatures from db
+app.get("/api/signatures", async (_, res) => {
+  try {
+    const signatures = await db("Signature")
+    res.status(200).json(signatures)
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 const PORT = 8787
